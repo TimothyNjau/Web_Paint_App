2f4f1b2d4799961573d38beb96fe3e2ae7fdbbd6
TimothyNjau <93652350+TimothyNjau@users.noreply.github.com>
2023-03-07 08:09:46 +0300

    Update 1.02.2
    *Added functionality to certain buttons: rectangle, circle, triangle, eraser.
    * Replaced toolbar buttons with, New and Save. Both with added functionality.
    * Fixed <<line>> button issue.

--- static/src/script.js
+++ static/src/script.js
@@ -6,12 +6,17 @@ let canvas, context,
    canvas_layout, //width & height of canvas 
    flag = false,
    drop_flag = false,
-   rangeSlide, outputDiv, thickness;
+   rangeSlide, outputDiv, thickness,
+   canvasImage, Save,New;
 
 
 let mouseX = 0, mouseY = 0, existingLines = [];
 
-let isDrawing = false;
+let Circle = false,
+   Line = false,
+   Triangle = false,
+   Freestyle = false,
+   Rectangle = false;
 
 var x = 'black', //default color of line drawn
    y = 2; // default line thicknes
@@ -28,6 +33,10 @@ function canv_init() {
    rangeSlide = document.querySelector("#rangeSlider");
    outputDiv = document.querySelector("#output");
    slideChange();
+   Save = document.querySelector("#save");
+   Save.addEventListener('click',saveCanvas,false);
+   New = document.querySelector('#new');
+   New.addEventListener('click',newCanvas,false);
 }
 
 function slideChange() {
@@ -39,13 +48,14 @@ function slideChange() {
    });
 }
 
-/*function changeThickness(){
-   slideChange();
-   thickness = rangeSlide.value;
-   console.log(thickness);
-   return thickness;
-}*/
-
+function chooseListener(func) {
+   if (Circle || Freestyle || Line || Triangle || Rectangle) {
+      canvas.addEventListener('mousemove', function (e) { func('move', e) }, false);
+      canvas.addEventListener('mousedown', function (e) { func('down', e) }, false);
+      canvas.addEventListener('mouseup', function (e) { func('up', e) }, false);
+      canvas.addEventListener('mouseout', function (e) { func('out', e) }, false);
+   }
+}
 function colorClick(choice) {
    switch (choice.id) {
       case 'red':
@@ -73,34 +83,40 @@ function colorClick(choice) {
 
 function freeStyle() {
    // add mouse event listeners
-   canvas.addEventListener('mousemove', function (e) { findxy('move', e) }, false);
-   canvas.addEventListener('mousedown', function (e) { findxy('down', e) }, false);
-   canvas.addEventListener('mouseup', function (e) { findxy('up', e) }, false);
-   canvas.addEventListener('mouseout', function (e) { findxy('out', e) }, false);
-}
-
-function findxy(motion, e) {
-   if (motion == 'down') {
-      prevX = currX;
-      prevY = currY;
-      getCursorCoordinates(e);
-      flag = true;
-      drop_flag = true;
-      if (drop_flag) {
-         draw();
-      }
-   } else if (motion == 'move') {
-      if (flag) {
-         prevX = currX, prevY = currY;
+   Freestyle = true;
+   Line = Circle = false;
+   chooseListener(findxy);
+   function findxy(motion, e) {
+      if (motion == 'down') {
+         prevX = currX;
+         prevY = currY;
          getCursorCoordinates(e);
-         context.moveTo(prevX, prevY);
-         context.lineTo(currX, currY);
-         context.strokeStyle = x;
-         context.lineWidth = y;
-         context.stroke();
+         flag = true;
+         drop_flag = true;
+         if (drop_flag) {
+            draw();
+         }
+      } else if (motion == 'move') {
+         if (flag) {
+            prevX = currX, prevY = currY;
+            getCursorCoordinates(e);
+            context.moveTo(prevX, prevY);
+            context.lineTo(currX, currY);
+            context.strokeStyle = x;
+            context.lineWidth = y;
+            context.stroke();
+         }
+      } else if (motion == 'up' || motion == 'out') {
+         flag = false;
       }
-   } else if (motion == 'up' || motion == 'out') {
-      flag = false;
+   }
+
+   function draw() {
+      context.beginPath();
+      context.fillStyle = x;
+      context.fillRect(currX, currY, y, 2);
+      context.closePath();
+      drop_flag = false;
    }
 }
 
@@ -111,22 +127,43 @@ function getCursorCoordinates(e) {
    return { currX: currX, currY: currY };
 }
 
-function draw() {
-   context.beginPath();
-   context.fillStyle = x;
-   context.fillRect(currX, currY, y, 2);
-   context.closePath();
-   drop_flag = false;
-}
-
-
 function drawLine() {
+   Line = true;
+   Freestyle = Circle = false;
+   chooseListener(draw_Line);
+   function draw_Line(motion, e) {
+      if (motion == 'down') {
+         getImage();
+         if (!flag) {
+            getCursorCoordinates(e);
+            prevX = currX;
+            prevY = currY;
+            flag = true;
+         }
 
-   canvas.addEventListener('mousemove', function (e) { mouseMove(e) }, false);
-   canvas.addEventListener('mousedown', function (e) { mouseDown(e) }, false);
-   canvas.addEventListener('mouseup', function (e) { mouseUp(e) }, false);
-   canvas.addEventListener('mouseout', function (e) { mouseOut(e) }, false);
-
+         draw_L();
+      } else if (motion == 'move') {
+         mouseX = e.clientX - canvas.offsetLeft;
+         mouseY = e.clientY - canvas.offsetTop;
+         console.log(mouseX, mouseY);
+         putImage();
+         draw_L();
+      } else if (motion == 'up') {
+         if (flag) {
+            existingLines.push({
+               startX: prevX,
+               startY: prevY,
+               endX: mouseX,
+               endY: mouseY
+            })
+            flag = false;
+         }
+         //putImage();
+         draw_L();
+      } else if (motion == 'out') {
+         flag = false;
+      }
+   }
    function draw_L() {
       //context.fillStyle = x;
       context.fillRect(currX, currY, y, 2);
@@ -139,9 +176,7 @@ function drawLine() {
          context.moveTo(line.startX, line.startY);
          context.lineTo(line.endX, line.endY);
       }
-
       context.stroke();
-
       if (flag) {
          context.strokeStyle = x;
          context.beginPath();
@@ -150,103 +185,141 @@ function drawLine() {
          context.stroke();
       }
    }
-   function mouseDown(e) {
-      if (!flag) {
-         getCursorCoordinates(e);
-         prevX = currX;
-         prevY = currY;
-         flag = true;
-      }
-      draw_L();
-   }
-   function mouseUp(e) {
-      if (flag) {
-         existingLines.push({
-            startX: prevX,
-            startY: prevY,
-            endX: mouseX,
-            endY: mouseY
-         })
+}
+function erasefunc() {
+   x = "white";
+   y = 10;
+   canvas.style.cursor = "crosshair";
+   freeStyle();
+}
+function drawCircle() {
+   Circle = true;
+   Freestyle = Line = false;
+   chooseListener(draw_Circle);
+   let tempX, tempY, radius;
+
+   function draw_Circle(motion, e) {
+      if (motion == 'down') {
+         if (!flag) {
+            startLocation = getCursorCoordinates(e);
+            flag = true;
+            getImage();
+         }
+      } else if (motion == 'up') {
+         flag = false;
+         pos = getCursorCoordinates(e);
+         putImage(); //this make sures that the image is not dragged.
+         draw_C(pos);
+      } else if (motion == 'move') {
+         if (flag) {
+            putImage();
+            let pos = getCursorCoordinates(e);
+            draw_C(pos);
+         }
+      } else if (motion == 'out') {
          flag = false;
       }
-      draw_L();
    }
-   function mouseMove(e) {
-      mouseX = e.clientX - canvas.offsetLeft;
-      mouseY = e.clientY - canvas.offsetTop;
-      console.log(mouseX, mouseY);
-      /*if(flag){
-         context.beginPath();
-         context.moveTo(prevX, prevY);
-         context.lineTo(mouseX, mouseY);
-         context.stroke();
-      } */
-   }
-   function mouseOut(e) {
-      flag = false;
-   }
-}
-
-/**
-function windmillDraw(){
-   canv_init();
-   
-   canvas.addEventListener('mousemove', function (e){mouseMove(e)}, false );
-   canvas.addEventListener('mousedown', function (e){mouseDown(e)}, false );
-   canvas.addEventListener('mouseup', function (e){mouseUp(e)}, false );
-   canvas.addEventListener('mouseout', mouseOut(e), false );
-
-function draw_L(){
-   context.fillStyle = x;
-   context.fillRect(currX,currY,y,2);
-   context.strokeStyle = x;
-   context.lineWidth = y;
-   context.beginPath();
-
-   for(var i=0;i< existingLines.length;++i){
-      let line = existingLines[i];
-      context.moveTo(line.startX,line.startY);
-      context.lineTo(line.endX,line.endY);
-   }
-
-   context.stroke();
-
-   if (isDrawing){
-      context.strokeStyle = 'red';
+   function draw_C(pos) {
+      tempX = startLocation.currX;
+      tempY = startLocation.currY;
+      context.strokeStyle = x;
+      context.lineWidth = y;
+      radius = Math.sqrt(Math.pow((tempX - pos.currX), 2) + Math.pow((tempY - pos.currY), 2));
       context.beginPath();
-      context.moveTo(prevX,prevY);
-      context.lineTo(currX,currY);
+      context.arc(tempX, tempY, radius, 0, 2 * Math.PI, false);
       context.stroke();
    }
 }
-function mouseDown(e){
-   if(!isDrawing){
-      getCursorCoordinates(e);
-      prevX = currX;
-      prevY = currY;
-      isDrawing = true;
-   }
-   draw_L();  
+function getImage() {
+   canvasImage = context.getImageData(0, 0, canvas.width, canvas.height);
 }
-function mouseUp(e){
-   if (isDrawing){
-      existingLines.push({
-         startX: prevX,
-         startY:prevY,
-         endX:currX,
-         endY:currY
-      })
-      isDrawing = false;
-   }
-   draw_L();
+function putImage() {
+   context.putImageData(canvasImage, 0, 0);
 }
-function mouseMove(e){
-   getCursorCoordinates(e);
-   if(isDrawing){
-      draw_L();
+function drawTriangle() {
+   Triangle = true;
+   Freestyle = Circle = Line = false;
+   chooseListener(draw_Triangle);
+   var tempPos;
+   function draw_Triangle(motion, e) {
+      if (motion == 'down') {
+         if (!flag) {
+            tempPos = getCursorCoordinates(e);
+            getImage();
+            flag = true;
+         }
+      } else if (motion == 'up') {
+         flag = false;
+         putImage();
+         draw_T();
+      } else if (motion == 'move') {
+         if (flag) {
+            putImage();
+            prevX = tempPos.currX;
+            prevY = tempPos.currY;
+            getCursorCoordinates(e);
+            draw_T();
+         }
+      } else if (motion == 'out') {
+         flag = false;
+      }
+   }
+   function draw_T() {
+      context.lineWidth = y;
+      context.strokeStyle = x;
+      context.beginPath();
+      context.moveTo(prevX, prevY);
+      context.lineTo(currX, currY);
+      context.lineTo(prevX * 2 - currX, currY);
+      context.closePath();
+      context.stroke();
+   }
+}
+function drawRectangle() {
+   Rectangle = true;
+   chooseListener(draw_Rectangle);
+   let tempPos;
+   function draw_Rectangle(motion, e) {
+      if (motion == 'down') {
+         if (!flag) {
+            tempPos = getCursorCoordinates(e);
+            flag = true;
+            getImage();
+         }
+      } else if (motion == 'move') {
+         if (flag) {
+            prevX = tempPos.currX, prevY = tempPos.currY;
+            getCursorCoordinates(e);
+            putImage();
+            draw_Rect();
+         }
+
+      } else if (motion == 'up') {
+         flag = false;
+         getCursorCoordinates(e);
+         putImage();
+         draw_Rect();
+      } else if (motion == 'out') {
+         flag = false;
+      }
+   }
+   function draw_Rect() {
+      context.beginPath();
+      context.lineWidth = y;
+      context.strokeStyle = x;
+      context.strokeRect(currX,currY,prevX-currX,prevY-currY);
+      context.closePath();
    }
 }
 
+function saveCanvas(){
+   let link = document.createElement("a"); 
+   link.download = `${Date.now()}.jpg`; 
+    link.href = canvas.toDataURL(); 
+    link.click();
 }
+function newCanvas(){
+   //context.clearRect(0, 0, canvas.width, canvas.height);
+   location.reload();
 }
-*/
